25fcf54c3e50891150d5b2fd755627f0ddac4bed
saijyo12 <52904345+saijyo12@users.noreply.github.com>
2026-09-03 18:00:31 +0530

    removed unused imports

--- src/main/java/com/auditlogservice/controller/AuditLogController.java
+++ src/main/java/com/auditlogservice/controller/AuditLogController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.validation.annotation.Validated;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
